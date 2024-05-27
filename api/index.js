e216533a506f96f9b4a5d4bec2e5eb8519deca0d
FrontendFlair <hamaadafzal404@gmail.com>
2024-05-27 00:32:49 -0700

    connect database and create user model

--- api/index.js
+++ api/index.js
@@ -1,14 +1,25 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 
-const cors = require('cors') 
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 app.use(cors());
+app.use(express.json());
 
-app.post('/register', (req, res) => {
+mongoose
+    .connect("mongodb://127.0.0.1:27017/MyBlogApp")
+    .then(() => {
+        console.log("Database connected successfully!");
+    })
+    .catch((error) => {
+        console.error("Database connection error:", error);
+    });
+
+app.post("/register", (req, res) => {
     res.status(201).json("OK!");
-})
+});
 
 app.listen(4000, () => {
-    console.log('Server is running on port : 4000');
-})
+    console.log("Server is running on port : 4000");
+});

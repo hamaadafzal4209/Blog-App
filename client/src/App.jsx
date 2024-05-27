00165eb49f0bdebd45fb48a67b5c7c73599a4cee
FrontendFlair <hamaadafzal404@gmail.com>
2024-05-26 22:37:41 -0700

    Add react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import Navbar from "./Components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
 
 function App() {
   return (
-    <div className='h-screen bg-indigo-900'>
-      
+    <div>
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

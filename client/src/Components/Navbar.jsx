036e86b8cb3551aa7d4a92f231345ed18a9844ba
FrontendFlair <hamaadafzal404@gmail.com>
2024-05-27 20:15:48 -0700

    Add context in navbar

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,26 +1,38 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { UserContext } from "../context/UserContext";
 
 function Navbar() {
-  const { userInfo,setUserInfo } = useContext(UserContext);
+  const { userInfo, setUserInfo } = useContext(UserContext);
 
   useEffect(() => {
     fetch("http://localhost:4000/profile", {
       credentials: "include",
-    }).then((response) => {
-      response.json().then((userInfo) => {
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Failed to fetch profile");
+      })
+      .then((userInfo) => {
         setUserInfo(userInfo);
+      })
+      .catch((error) => {
+        console.error("Error fetching profile:", error);
       });
-    });
   }, [setUserInfo]);
 
-  const handleLogout = () => {
-    fetch("http://localhost:4000/logout", {
-      credentials: "include",
-      method: "POST",
-    });
-    setUserInfo(null);
+  const handleLogout = async () => {
+    try {
+      await fetch("http://localhost:4000/logout", {
+        credentials: "include",
+        method: "POST",
+      });
+      setUserInfo(null);
+    } catch (error) {
+      console.error("Error logging out:", error);
+    }
   };
 
   const username = userInfo?.username;
@@ -36,7 +48,12 @@ function Navbar() {
             <Link to="/create">Create New Post</Link>
           </li>
           <li onClick={handleLogout} className="text-lg font-semibold">
-            Logout
+            <button
+              type="button"
+              className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+            >
+              Logout
+            </button>
           </li>
         </ul>
       ) : (
